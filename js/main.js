2b214f73bc22cd0871f596e909f452bd5b64d5c1
UmbertoDV <biccoumby2002@hotmail.com>
2023-02-20 16:14:24 +0100

    header placeholder

--- js/main.js
+++ js/main.js
@@ -3,7 +3,32 @@ const { createApp } = Vue;
 createApp({
 	data() {
 		return {
-			message: "Hello Vue!",
+			headerObj: {
+				images: [
+					{
+						cocaCola: "./img/assets/sponsor1.png",
+						nike: "./img/assets/sponsor2.png",
+						footballClub: "./img/assets/loader-football.png",
+						facebook: "./img/assets/facebook.svg",
+						instagram: "./img/assets/instagram.svg",
+						twitter: "./img/assets/twitter.svg",
+					},
+				],
+				text: [
+					{
+						first: "HOME",
+						second: "FIXTURES & RESULT",
+						third: "LEAGUE TABLE",
+						fourth: "PLAYERS",
+						fifth: "GALLERY",
+						sixth: "BLOG",
+						seventh: "CONTACT",
+					},
+				],
+			},
 		};
+	},
+	created() {
+		console.log(this.headerObj);
 	},
 }).mount("#app");
